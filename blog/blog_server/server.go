595d0b0d6dc3e7eefc443a1d0acf71d9cc4ddce8
Joshua Edwards <josh.dean.edwards@gmail.com>
2021-03-25 06:06:43 -0500

    add connection to mongodb

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,30 +1,52 @@
 package main
 
 import (
+  "context"
   "fmt"
   "github.com/pandadragoon/grpc-go-course/blog/blogpb"
+  "go.mongodb.org/mongo-driver/bson/primitive"
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
   "google.golang.org/grpc"
   "log"
   "net"
   "os"
   "os/signal"
+  "time"
 )
 
 type server struct {}
+
+type blogItem struct {
+  ID       primitive.ObjectID `bson:"_id,omitempty"`
+  AuthorID string             `bson:"author_id"`
+  Content  string             `bson:"content"`
+  Title    string             `bson:"title"`
+}
+
+var collection *mongo.Collection
 
 func main() {
   // Get file name and line number if code crashes
   log.SetFlags(log.LstdFlags | log.Lshortfile)
   fmt.Println("Starting blog server...")
+
+  ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+  defer cancel()
+  fmt.Println("Connecting to db...")
+  client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+  if err != nil { log.Fatalf("Error connecting to database %v", err) }
+
+  collection = client.Database("mydb").Collection("blog")
+
   host := "0.0.0.0"
   port := ":50051"
-
   lis, err := net.Listen("tcp", host + port)
   if err != nil {
     log.Fatalf("Failed to listen: %v", err)
   }
 
-  fmt.Printf("Listening at %s on port %s", )
+  fmt.Printf("Listening at %s on port %s", host, port)
 
   var opts []grpc.ServerOption
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.4.3 // indirect
+	go.mongodb.org/mongo-driver v1.5.0 // indirect
 	google.golang.org/grpc v1.36.0 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
