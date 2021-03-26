f2d0638b47bf1f0d61a5cb4078c04a6d8164a023
Joshua Edwards <josh.dean.edwards@gmail.com>
2021-03-26 08:40:17 -0500

    finished blog CRUD actions

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -6,11 +6,9 @@ import (
 	"github.com/pandadragoon/grpc-go-course/blog/blogpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
+	"io"
 	"log"
 )
-
-// * Use ID From local DB
-var readID = "605d096980069b683195f071"
 
 func main() {
 	fmt.Println("Starting blog client...")
@@ -27,11 +25,15 @@ func main() {
 	fmt.Println("Connected to server!")
 
 	c := blogpb.NewBlogServiceClient(cc)
-	createBlog(c)
-	readBlog(c, readID)
+	blog := createBlog(c)
+	blogID := blog.GetId()
+	readBlog(c, blogID)
+	updateBlog(c, blogID)
+	deleteBlog(c, blogID)
+	listBlog(c)
 }
 
-func createBlog(c blogpb.BlogServiceClient) {
+func createBlog(c blogpb.BlogServiceClient) *blogpb.Blog {
 	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: &blogpb.Blog{
 		AuthorId: "22",
 		Title:    "The Chill",
@@ -45,10 +47,12 @@ func createBlog(c blogpb.BlogServiceClient) {
 		} else {
 			log.Fatalf("Unknown err: %v", err)
 		}
-		return
 	}
 
-	log.Printf("Saved %v", res.GetBlog())
+	blog := res.GetBlog()
+	log.Printf("Saved %v", blog)
+
+	return blog
 }
 
 func readBlog(c blogpb.BlogServiceClient, blogID string) {
@@ -61,4 +65,44 @@ func readBlog(c blogpb.BlogServiceClient, blogID string) {
 	}
 
 	fmt.Printf("Blog was read: %v \n", readBlogRes)
+}
+
+func updateBlog(c blogpb.BlogServiceClient, blogID string) {
+	newBlog := &blogpb.Blog{
+		Id:       blogID,
+		AuthorId: "Changed Author",
+		Title:    "My First Blog (edited)",
+		Content:  "Content of the first blog, with some awesome additions!",
+	}
+	updateRes, updateErr := c.UpdateBlog(context.Background(), &blogpb.UpdateBlogRequest{Blog: newBlog})
+	if updateErr != nil {
+		fmt.Printf("Error happened while updating: %v \n", updateErr)
+	}
+	fmt.Printf("Blog was updated: %v\n", updateRes)
+}
+
+func deleteBlog(c blogpb.BlogServiceClient, blogID string) {
+	deleteRes, deleteErr := c.DeleteBlog(context.Background(), &blogpb.DeleteBlogRequest{BlogId: blogID})
+
+	if deleteErr != nil {
+		fmt.Printf("Error happened while deleting: %v \n", deleteErr)
+	}
+	fmt.Printf("Blog was deleted: %v \n", deleteRes)
+}
+
+func listBlog(c blogpb.BlogServiceClient) {
+	stream, err := c.ListBlog(context.Background(), &blogpb.ListBlogRequest{})
+	if err != nil {
+		log.Fatalf("error while calling ListBlog RPC: %v", err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Something happened: %v", err)
+		}
+		fmt.Println(res.GetBlog())
+	}
 }
--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -102,6 +102,111 @@ func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*
 	}, nil
 }
 
+func (*server) UpdateBlog(ctx context.Context, req *blogpb.UpdateBlogRequest) (*blogpb.UpdateBlogResponse, error) {
+	fmt.Println("Update blog request")
+	blog := req.GetBlog()
+	oid, err := primitive.ObjectIDFromHex(blog.GetId())
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("Cannot parse ID"),
+		)
+	}
+
+	// create an empty struct
+	data := &blogItem{}
+	filter := bson.M{"_id": oid}
+
+	res := collection.FindOne(context.Background(), filter)
+	if err := res.Decode(data); err != nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintf("Cannot find blog with specified ID: %v", err),
+		)
+	}
+
+	// we update our internal struct
+	data.AuthorID = blog.GetAuthorId()
+	data.Content = blog.GetContent()
+	data.Title = blog.GetTitle()
+
+	_, updateErr := collection.ReplaceOne(context.Background(), filter, data)
+	if updateErr != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Cannot update object in MongoDB: %v", updateErr),
+		)
+	}
+
+	return &blogpb.UpdateBlogResponse{
+		Blog: dataToBlogPb(data),
+	}, nil
+
+}
+
+func (*server) DeleteBlog(ctx context.Context, req *blogpb.DeleteBlogRequest) (*blogpb.DeleteBlogResponse, error) {
+	fmt.Println("Delete blog request")
+	oid, err := primitive.ObjectIDFromHex(req.GetBlogId())
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("Cannot parse ID"),
+		)
+	}
+
+	filter := bson.M{"_id": oid}
+
+	res, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Cannot delete object in MongoDB: %v", err),
+		)
+	}
+
+	if res.DeletedCount == 0 {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintf("Cannot find blog in MongoDB: %v", err),
+		)
+	}
+
+	return &blogpb.DeleteBlogResponse{BlogId: req.GetBlogId()}, nil
+}
+
+func (*server) ListBlog(req *blogpb.ListBlogRequest, stream blogpb.BlogService_ListBlogServer) error {
+	fmt.Println("List blog request")
+
+	cur, err := collection.Find(context.Background(), primitive.D{{}})
+	if err != nil {
+		return status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Unknown internal error: %v", err),
+		)
+	}
+	defer cur.Close(context.Background())
+	for cur.Next(context.Background()) {
+		data := &blogItem{}
+		err := cur.Decode(data)
+		if err != nil {
+			return status.Errorf(
+				codes.Internal,
+				fmt.Sprintf("Error while decoding data from MongoDB: %v", err),
+			)
+
+		}
+		stream.Send(&blogpb.ListBlogResponse{Blog: dataToBlogPb(data)})
+	}
+	if err := cur.Err(); err != nil {
+		return status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Unknown internal error: %v", err),
+		)
+	}
+	return nil
+}
+
 func (*server) ReadBlog(ctx context.Context, req *blogpb.ReadBlogRequest) (*blogpb.ReadBlogResponse, error) {
 	blogId := req.GetBlogId()
 	oid, err := primitive.ObjectIDFromHex(blogId)
